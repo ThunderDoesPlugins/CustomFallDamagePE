c2550e7f1419b05be66e4eeb846d71e275c1bc61
Thunder33345 <Thunder33345@users.noreply.github.com>
2020-03-17 00:51:35 +0800

    init

---
+++ src/Thunder33345/CustomFallDamagePE/CustomFallDamage.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+/** Created By Thunder33345 **/
+
+namespace Thunder33345\CustomFallDamagePE;
+
+use pocketmine\event\entity\EntityDamageEvent;
+use pocketmine\plugin\PluginBase;
+
+class CustomFallDamage extends PluginBase
+{
+	public function onEnable()
+	{
+		$this->saveDefaultConfig();
+	}
+
+	public function onFall(EntityDamageEvent $event)
+	{
+		if($event->getCause() !== EntityDamageEvent::CAUSE_FALL)
+			return;
+		$worldName = $event->getEntity()->getLevel()->getName();
+
+		$multiplier = $this->getConfig()->getNested('multiplier.' . $worldName, $this->getConfig()->get('default-multiplier'));
+		$total = $event->getBaseDamage() * $multiplier;
+
+		$sum = $this->getConfig()->getNested('sum.' . $worldName, $this->getConfig()->get('default-sum'));
+		$total = $total + $sum;
+
+		$cutoff = $this->getConfig()->getNested('cutoff.' . $worldName, $this->getConfig()->get('default-cutoff'));
+
+		if($cutoff >= $total)
+			$event->setBaseDamage(0);
+		else
+			$event->setBaseDamage($total);
+	}
+}
